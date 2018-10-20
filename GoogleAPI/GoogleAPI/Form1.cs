2539b2b9e8471297809f4f21f5486643117cbfdd
misha1020 <mishaz020@mail.ru>
2018-10-20 13:40:20 +0500

    Inicialization
    Первые шаги.

---
+++ GoogleAPI/GoogleAPI/Form1.cs
@@ -0,0 +1,88 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoogleAPI
+{
+    public partial class Form1 : Form
+    {
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void OAuth2()
+        {
+            UserCredential credential;
+            textBox1.Text = "";
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                //Console.WriteLine("Credential file saved to: " + credPath);
+                textBox1.Text += "Credential file saved to: " + credPath + '\n';
+            }
+
+            // Create Google Calendar API service.
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Google Calendar API",
+            });
+
+            // Define parameters of request.
+            EventsResource.ListRequest request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            // List events.
+            Events events = request.Execute();
+            //Console.WriteLine("Upcoming events:");
+            textBox1.Text += "Upcoming events:" + '\n';
+            if (events.Items != null && events.Items.Count > 0)
+            {
+                foreach (var eventItem in events.Items)
+                {
+                    string when = eventItem.Start.DateTime.ToString();
+                    if (String.IsNullOrEmpty(when))
+                    {
+                        when = eventItem.Start.Date;
+                    }
+                    //Console.WriteLine("{0} ({1})", eventItem.Summary, when);
+                    textBox1.Text += eventItem.Summary + '\n' + when;
+                }
+            }
+            else
+            {
+                //Console.WriteLine("No upcoming events found.");
+                textBox1.Text += "No upcoming events found.";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OAuth2();
+        }
+    }
+}
