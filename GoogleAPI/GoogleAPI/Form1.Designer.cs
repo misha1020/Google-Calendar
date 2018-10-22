b51b1451512075cafd483e7c79ba4c9aef5709e4
misha1020 <mishaz020@mail.ru>
2018-10-22 15:30:57 +0500

    Auth, Read, Delete, Insert (50%)
    1. Auth 2.0.
    2. Read to listview.
    3. Delete selectem element in listview.
    4. Insert without formInsert.

--- GoogleAPI/GoogleAPI/Form1.Designer.cs
+++ GoogleAPI/GoogleAPI/Form1.Designer.cs
@@ -28,46 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lvEvents = new System.Windows.Forms.ListView();
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chStartTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chFinishTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btDelete = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(282, 269);
-            this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(300, 10);
+            this.button1.Location = new System.Drawing.Point(373, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.Size = new System.Drawing.Size(91, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Авторизация";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(373, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Выход из УЗ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lvEvents
+            // 
+            this.lvEvents.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chStartTime,
+            this.chFinishTime});
+            this.lvEvents.FullRowSelect = true;
+            this.lvEvents.Location = new System.Drawing.Point(12, 12);
+            this.lvEvents.MultiSelect = false;
+            this.lvEvents.Name = "lvEvents";
+            this.lvEvents.Size = new System.Drawing.Size(355, 280);
+            this.lvEvents.TabIndex = 3;
+            this.lvEvents.UseCompatibleStateImageBehavior = false;
+            this.lvEvents.View = System.Windows.Forms.View.Details;
+            // 
+            // chName
+            // 
+            this.chName.Text = "Name";
+            this.chName.Width = 45;
+            // 
+            // chStartTime
+            // 
+            this.chStartTime.Text = "Start Time";
+            this.chStartTime.Width = 62;
+            // 
+            // chFinishTime
+            // 
+            this.chFinishTime.Text = "Finish Time";
+            this.chFinishTime.Width = 71;
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(392, 86);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(75, 23);
+            this.btDelete.TabIndex = 4;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(405, 157);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(553, 324);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.lvEvents);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView lvEvents;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chStartTime;
+        private System.Windows.Forms.ColumnHeader chFinishTime;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- GoogleAPI/GoogleAPI/Form1.cs
+++ GoogleAPI/GoogleAPI/Form1.cs
@@ -16,33 +16,42 @@ namespace GoogleAPI
 {
     public partial class Form1 : Form
     {
-        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string[] Scopes = { CalendarService.Scope.CalendarEvents };
+        UserCredential credential;
+        CalendarService service;
 
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        public void Read()
+        {
+
         }
 
         public void OAuth2()
         {
-            UserCredential credential;
-            textBox1.Text = "";
             using (var stream =
                 new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
             {
                 string credPath = "token.json";
                 credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-                //Console.WriteLine("Credential file saved to: " + credPath);
-                textBox1.Text += "Credential file saved to: " + credPath + '\n';
+                GoogleClientSecrets.Load(stream).Secrets,
+                Scopes,
+                "user",
+                CancellationToken.None,
+                new FileDataStore(credPath, true)).Result;
+                MessageBox.Show("Credential file saved!");
             }
+        }
 
+        private void UpdateList()
+        {
+            lvEvents.Items.Clear();
             // Create Google Calendar API service.
-            var service = new CalendarService(new BaseClientService.Initializer()
+            service = new CalendarService(new BaseClientService.Initializer()
             {
                 HttpClientInitializer = credential,
                 ApplicationName = "Google Calendar API",
@@ -53,36 +62,105 @@ namespace GoogleAPI
             request.TimeMin = DateTime.Now;
             request.ShowDeleted = false;
             request.SingleEvents = true;
-            request.MaxResults = 10;
+            request.MaxResults = 100;
             request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
 
             // List events.
             Events events = request.Execute();
-            //Console.WriteLine("Upcoming events:");
-            textBox1.Text += "Upcoming events:" + '\n';
             if (events.Items != null && events.Items.Count > 0)
             {
                 foreach (var eventItem in events.Items)
                 {
-                    string when = eventItem.Start.DateTime.ToString();
-                    if (String.IsNullOrEmpty(when))
+                    string start = eventItem.Start.DateTime.ToString();
+                    string end = eventItem.End.DateTime.ToString();
+                    if (String.IsNullOrEmpty(start))
                     {
-                        when = eventItem.Start.Date;
+                        start = eventItem.Start.Date;
                     }
-                    //Console.WriteLine("{0} ({1})", eventItem.Summary, when);
-                    textBox1.Text += eventItem.Summary + '\n' + when;
+                    if (String.IsNullOrEmpty(end))
+                    {
+                        end = eventItem.Start.Date;
+                    }
+                    var newItem = new ListViewItem(new string[] { eventItem.Summary, start, end });
+                    newItem.Tag = eventItem.Id;
+                    lvEvents.Items.Add(newItem);
                 }
+                lvEvents.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+
             }
             else
             {
-                //Console.WriteLine("No upcoming events found.");
-                textBox1.Text += "No upcoming events found.";
+                MessageBox.Show("No upcoming events found.");
             }
+        }
+
+        private void AddEvent()
+        {
+
+            Event newEvent = new Event()
+            {
+                Summary = "New SANjsf",
+                Location = "800 Howard St., San Francisco, CA 94103",
+                Description = "asjhfkajshf"
+            };
+
+            DateTime startTime = new DateTime(2018, 10, 22, 16, 30, 0);
+            EventDateTime start = new EventDateTime()
+            {
+                DateTime = startTime,
+                TimeZone = "America/Los_Angeles"
+            };
+            newEvent.Start = start;
+
+            DateTime endTime = new DateTime(2018, 10, 22, 18, 30, 0);
+            EventDateTime end = new EventDateTime()
+            {
+                DateTime = endTime,
+                TimeZone = "America/Los_Angeles"
+            };
+            newEvent.End = end;
+
+            service.Events.Insert(newEvent, "primary").Execute();
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            OAuth2();
+            try
+            {
+                OAuth2();
+                UpdateList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка аутентификации");
+                lvEvents.Items.Clear();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GoogleWebAuthorizationBroker.ReauthorizeAsync(credential, CancellationToken.None);
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            // Delete an event
+            if (lvEvents.SelectedItems.Count != 0)
+            {
+                service.Events.Delete("primary", lvEvents.SelectedItems[0].Tag.ToString()).Execute();
+                lvEvents.Items.Remove(lvEvents.SelectedItems[0]);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AddEvent();
+
+            UpdateList();
+
         }
     }
 }
